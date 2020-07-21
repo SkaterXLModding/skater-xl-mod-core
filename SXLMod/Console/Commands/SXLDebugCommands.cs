d860e79ade0979a555d147c17cff552dc8a1d612
Greg Amato <amatobahn@gmail.com>
2020-07-20 20:07:06 -0500

    1.0 updates

--- SXLMod/Console/Commands/SXLDebugCommands.cs
+++ SXLMod/Console/Commands/SXLDebugCommands.cs
@@ -178,5 +178,37 @@ namespace SXLMod.Console
             }
             Debug.Log($"<b>TOTAL</b>: {string.Join(" | ", totals)}");
         }
+        
+        [RegisterCommand(Name = "d_fixhdrp", Help = "Run HDRP Fix on current map", Hint = "d_fixhdrp", ArgMax=0)]
+        static void CommandFixHDRP(CommandArg[] args)
+        {
+            Shader hdrpLit = Shader.Find("HDRP/Lit");
+            Shader hdrpDecal = Shader.Find("HDRP/Decal");
+            Material debugMat = new Material(hdrpLit);
+            debugMat.color = Color.red;
+
+            Debug.Log("Running Shader Fixes...");
+            foreach (Renderer renderer in UnityEngine.Object.FindObjectsOfType<Renderer>())
+            {
+                Material[] sharedMaterials = renderer.sharedMaterials;
+                for (int index = 0; index < sharedMaterials.Length; ++index)
+                {
+                    try
+                    {
+                        Shader s = sharedMaterials[index].shader;
+                        bool hdrp = s.name.Contains("HDRP");
+                        bool legacyHDRP = s.name.Contains("HDRenderPipeline");
+                        sharedMaterials[index].shader = hdrp ? Shader.Find(s.name) : legacyHDRP ? s.name.Contains("Decal") ? hdrpDecal : hdrpLit : Shader.Find(s.name);
+                        continue;
+                    }
+                    catch
+                    {
+                        Debug.Log($"{renderer.gameObject.name} Has a material ({index}) that needs to be addressed.");
+                        renderer.material = debugMat;
+                    }
+                    continue;
+                }
+            }
+        }
     }
 }
